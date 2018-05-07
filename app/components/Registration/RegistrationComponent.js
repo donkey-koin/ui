6051d72285a106a96a046228e54b8c13da85365b
sowakarol <karosowa@student.agh.edu.pl>
2018-05-07 21:43:03 +0200

    Add modal with messages from server response

--- app/components/Registration/RegistrationComponent.js
+++ app/components/Registration/RegistrationComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Modal, Button } from 'react';
 import { Link } from 'react-router-dom';
 import validators from './validator';
 import * as registrationStyles from '../../styles/registration.scss';
@@ -15,7 +15,10 @@ export default class Registration extends Component {
             "email": '',
             "password-confirmation": '',
             "akzeptierung": '',
-            "isLoading": false
+            "isLoading": false,
+            "showModal": false,
+            'registrationError': null,
+            "successMessage": null
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -101,8 +104,20 @@ export default class Registration extends Component {
         });
     }
 
+    goBack = (event) => {
+        event.preventDefault();
+        this.state.showModal = false;
+        this.state.registrationError = null;
+        this.state.successMessage = null;
+        this.forceUpdate();
+    }
+
+    signIn = (event) => {
+        event.preventDefault();
+        this.props.history.push('/');
+    }  
+
     handleSubmit(event) {
-        console.log(this.state);
         console.log("form submitted");
         let body = {
             "username": this.state.username,
@@ -111,28 +126,43 @@ export default class Registration extends Component {
         }
         console.log(body);
         this.state.isLoading = true;
-        fetch('http://localhost:5000/register',{
+
+        fetch('http://localhost:5000/register', {
             body: JSON.stringify(body),
             headers: {
                 'content-type': 'application/json',
             },
             method: 'POST'
         })
-        .then(res => {
-            console.log(res);
-            if(res.error) {
-                console.log(res.error);
-            }
-            console.log('registered');
-        })
-        .catch(error => console.log(error));
+            .then(res => res.json())
+            .then(res => {
+                console.log(res);
+                this.state.isLoading = false;
+                this.state.showModal = true;
+                console.log(this.state);
+                if (res.error) {
+                    console.log(res.error);
+                    this.state.registrationError = res.error;
+                } else {
+                    this.state.successMessage = res.data;
+                }
+                console.log('registered');
+                console.log(this.state);
+                this.forceUpdate();
+            })
+            .catch(error => {
+                console.log(error);
+                this.state.showModal = true;
+                this.state.registrationError = error;
+            });
+
 
         event.preventDefault();
     }
 
     render() {
         return (
-            <div className={registrationStyles + " container-fluid"} >
+            <div className={registrationStyles.form + " container-fluid"} >
                 <form onSubmit={this.handleSubmit}>
                     <h1>Create new account</h1>
                     <h5>Join Donkey Koin Exchange.</h5>
@@ -143,7 +173,7 @@ export default class Registration extends Component {
                     </div>
                     <div className="form-group">
                         <label htmlFor="email">Email:</label>
-                        <input id="email" className="form-control" type="email" onBlur={(e) => this.handleInputChange(e, "email")}/>
+                        <input id="email" className="form-control" type="email" onBlur={(e) => this.handleInputChange(e, "email")} />
                         {this.displayValidationErrors("email")}
                     </div>
                     <div className="form-group">
@@ -158,9 +188,9 @@ export default class Registration extends Component {
                         {this.displayValidationErrors("password-confirmation")}
                     </div>
                     <div className="checkbox">
-                        <label><input id="akzeptierung" type="checkbox" onChange={(e) => this.handleInputChange(e, "akzeptierung")} /> I agree&nbsp;  
+                        <label><input id="akzeptierung" type="checkbox" onChange={(e) => this.handleInputChange(e, "akzeptierung")} /> I agree&nbsp;
                         <Link to="/legal/terms" target="_blank">Terms And Conditions</Link>
-                        &nbsp;and&nbsp; 
+                            &nbsp;and&nbsp;
                         <Link to="/legal/privacy" target="_blank">Privacy Policy</Link>.
                         </label>
                         {this.displayValidationErrors("akzeptierung")}
@@ -170,6 +200,35 @@ export default class Registration extends Component {
                         <Link to="/">Already have an account?</Link>
                     </div>
                 </form>
+                {
+                    this.state.showModal &&
+                    <div className={registrationStyles.modal}>
+                        <div className={registrationStyles.modalCtx}>
+                            <div className={registrationStyles.modalBody}>
+                                {
+                                    this.state.registrationError !== null
+                                        ?
+                                        <h3>
+                                            {this.state.registrationError}
+                                        </h3>
+                                        :
+                                        <h3>
+                                            {this.state.successMessage}
+                                        </h3>
+                                }
+                            </div>
+                            <div className={registrationStyles.modalFooter}>
+                                {
+                                    this.state.registrationError !== null
+                                        ?
+                                        <button type="button" className="btn btn-default" onClick={this.goBack}>Go Back</button>
+                                        :
+                                        <button type="button" className="btn btn-default" onClick={this.signIn}>Sign in</button>
+                                }
+                            </div>
+                        </div>
+                    </div>
+                }
             </div>
         )
     }
