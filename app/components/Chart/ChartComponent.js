ac38080c1d1f83b687f18e630f0890677dc4d414
sowakarol <karosowa@student.agh.edu.pl>
2018-05-08 00:07:54 +0200

    Add mockup of chart component

--- app/components/Chart/ChartComponent.js
+++ app/components/Chart/ChartComponent.js
@@ -1,18 +1,75 @@
 import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
+import * as chartStyles from '../../styles/chart.scss';
 
 
+const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'Value',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40]
+        }
+    ]
+};
+
+
+let options = {
+    scaleShowGridLines: true,
+    scaleGridLineColor: "rgba(0,0,0,.05)",
+    scaleGridLineWidth: 1,
+    scaleShowHorizontalLines: true,
+    scaleShowVerticalLines: true,
+    bezierCurve: true,
+    bezierCurveTension: 0.4,
+    pointDot: true,
+    pointDotRadius: 4,
+    pointDotStrokeWidth: 1,
+    pointHitDetectionRadius: 20,
+    datasetStroke: true,
+    datasetStrokeWidth: 2,
+    datasetFill: true,
+    legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"><%if(datasets[i].label){%><%=datasets[i].label%><%}%></span></li><%}%></ul>",
+    offsetGridLines: false
+};
 export class Chart extends Component {
-
 
     constructor(props) {
         super(props);
+        this.state = {
+            data: data,
+            width: 400,
+            height: 200
+        }
+        // console.log(this.state.data);
     }
 
-
+    // getInitialState() {
+    //     return {
+    //         data: data()
+    //     };
+    // }
     render() {
         return (
-            <div className="jumbotron"> 
-                <h1>Chart</h1>
+            <div className={chartStyles.chartDiv}>
+                <Line data={data} />
             </div>
         )
     }
