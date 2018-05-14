9b85a44e602ef545ce31e0440b4191db4f35f03a
Siorek, Oskar <oskar.siorek@sabre.com>
2018-05-15 00:32:13 +0200

    monkey on login

--- app/components/Login/LoginComponent.js
+++ app/components/Login/LoginComponent.js
@@ -3,6 +3,8 @@ import { login } from '../../actions';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router-dom';
 import * as loginStyles from '../../styles/login.scss';
+import * as navbarStyles from "../../styles/navbar.scss";
+import logo from '../../styles/logo.png';
 
 export default class Login extends Component { 
     constructor(props) {
@@ -49,8 +51,8 @@ export default class Login extends Component {
         return(
             <div className={loginStyles.loginScreen + " container-fluid"}>
                     <div className={loginStyles.container}>
-                        <div className={loginStyles.title + " page-header"}><h2>Donkey Koin Exchange</h2></div>
-                        <div className={loginStyles.loginForm}> 
+                        <div className={loginStyles.title + " page-header"}><h2><img src ={logo} style={{width: '10em', height: '3em'}} alt="Logo"/></h2></div>
+                        <div className={loginStyles.loginForm}>
                             <div className="row"><input type="text" onChange={this.handleLoginChange} placeholder="Username" /></div>
                             <div className="row"><input type="password" onChange={this.handlePasswordChange} placeholder="Password" /></div>
                             <div className="row">
